88059b5410452f4ebfd46785cfa29c6c75f9e951
fidelismailson27-rgb <fidelismailson27@gmail.com>
2026-07-08 10:14:25 -0300

    feat: Hilt-qualified coroutine dispatchers (T013)

---
+++ app/src/main/kotlin/com/circulesearch/app/di/DispatchersModule.kt
@@ -0,0 +1,41 @@
+package com.circulesearch.app.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
+
+/**
+ * Injectable coroutine dispatchers. [DefaultDispatcher] is where crop/downscale/WebP
+ * compression must run (constitution VI); [IoDispatcher] is for network/DataStore.
+ */
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatchersModule {
+    @Provides
+    @IoDispatcher
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @Provides
+    @DefaultDispatcher
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @Provides
+    @MainDispatcher
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+}
