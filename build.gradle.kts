f799101021980fb58b589a8602b0e31f29e8decf
fidelismailson27-rgb <fidelismailson27@gmail.com>
2026-07-08 09:29:31 -0300

    build: configure ktlint and detekt static analysis (T004)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,3 +10,20 @@ plugins {
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
 }
+
+// T004: static analysis, applied uniformly to every module (currently just :app).
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    extensions.configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        android.set(true)
+        ignoreFailures.set(false)
+    }
+
+    extensions.configure<io.gitlab.arturbosch.detekt.extensions.DetektExtension> {
+        buildUponDefaultConfig = true
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+        autoCorrect = false
+    }
+}
